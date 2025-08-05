c7ffa9adbd68c4607a77634a58e7b93d39851dc0
Asmita <120050701+Asmitajadhav12@users.noreply.github.com>
2025-08-05 23:32:45 +0530

    Update DBConnection.java

--- Smart-Placement-Tracker/DBConnection.java
+++ Smart-Placement-Tracker/DBConnection.java
@@ -1,1 +1,15 @@
 
+package tracker;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static Connection connect() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/placement_tracker";
+        String user = "root";      // your username
+        String password = "Jadhavasme1218@@";      // your password (blank if using XAMPP)
+        return DriverManager.getConnection(url, user, password);
+    }
+}
